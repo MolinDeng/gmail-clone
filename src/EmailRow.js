df64910d6c2ea0d76edc221a3fec45c9cd55a284
MolinDeng <zjumorning@gmail.com>
2023-05-13 16:14:13 +0800

    optimize email row click behavior

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -11,20 +11,26 @@ function EmailRow({ id, sender, subject, content, time, unread }) {
     navigate("/mail");
   };
   return (
-    <div onClick={onClick} className="email-row">
+    <div className="email-row">
       <div className="email-row-options">
         <Checkbox size="small" />
         <IconButton size="small">
           <StarBorderOutlined fontSize="small" />
         </IconButton>
       </div>
-      <h4 className={`email-row-sender${unread ? " unread" : ""}`}>{sender}</h4>
-      <h4 className={`email-row-sub${unread ? " unread" : ""}`}>{subject} </h4>
-      <p className="email-row-content">
-        {"- "}
-        {content}
-      </p>
-      <h4 className={`email-row-time${unread ? " unread" : ""}`}>{time}</h4>
+      <div onClick={onClick} className="email-row-main">
+        <h4 className={`email-row-sender${unread ? " unread" : ""}`}>
+          {sender}
+        </h4>
+        <h4 className={`email-row-sub${unread ? " unread" : ""}`}>
+          {subject}{" "}
+        </h4>
+        <p className="email-row-content">
+          {"- "}
+          {content}
+        </p>
+        <h4 className={`email-row-time${unread ? " unread" : ""}`}>{time}</h4>
+      </div>
     </div>
   );
 }
